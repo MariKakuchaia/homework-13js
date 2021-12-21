815c40171fe2c385752bbf921b9842d7cd5fb2cf
MariKakuchaia <94448857+MariKakuchaia@users.noreply.github.com>
2021-12-21 18:28:20 +0400

    Primitive types

---
+++ app.js
@@ -0,0 +1,12 @@
+const string = "string";
+const number = 19;
+const boolean1= true;
+const boolean2 = false;
+const nullis = null;
+const undefinedis=undefined;
+
+const myname="mari";
+const age=20;
+const hobby="cook";
+
+console.log(`My name is ${myname} I am ${age} years old and my hobby is ${hobby}.`)
